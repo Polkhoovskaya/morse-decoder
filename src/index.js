31d18d55d616c2b89ef6474d7cc69ac3c3bc85d0
VolhaP <polkhovskayaolga@mail.ru>
2019-10-10 19:02:15 +0300

    feat: return result

--- src/index.js
+++ src/index.js
@@ -38,9 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let result = '';
+    let arrOfElements = [];
+    let arrOfEncodedElements = [];
+    let arrOfMorseCode = [];
+    for (var i = 0; i < expr.length; i++) {
+        let j = Math.trunc(i/10);
+        arrOfElements[j] += expr[i];
+    }
+    arrOfElements.forEach(function(element){
+        arrOfEncodedElements.push(element.replace(undefined , '')); 
+    });
 
+    arrOfEncodedElements.forEach(function(element){
+        let code = '';
+        for (let i = 0; i < element.length; i++){
+            if (element[i] === '1' & element[i+1] === '0') {
+                code = code + '.';
+                i++;
+                continue;
+            } 
+            if (element[i] === '1' & element[i+1] === '1') {
+                code = code + '-';
+                i++;
+                continue;
+            } 
+            if (expr[i] === '*') {
+                code = code + ' ';
+                i+=10;
+                continue;
+            } 
+        }
+        arrOfMorseCode.push(code);
+    });
+
+    arrOfMorseCode.forEach(function(element){
+        if (element == ''){
+            result = result + ' ';
+        } else {
+            result = result + MORSE_TABLE[element];
+        }     
+    });
+
+    return result;
+}
 module.exports = {
     decode
 }
